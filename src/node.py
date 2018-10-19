94f240261e9b9ef6039c2d49ec2181f144185638
Eddie <ehwa009@aucklanduni.ac.nz>
2018-10-19 16:11:23 +1300

    initial commit

---
+++ src/node.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+#-*- encoding: utf8 -*-
+
+import os
+import json
+import numpy as np
+import rospy
+import rospkg
+
+from enum import Enum
+from mind_msgs.msg import Reply, RaisingEvents
+from mind_msgs.srv import ReadData, ReadDataResponse, WriteData, WriteDataResponse, RegisterData, RegisterDataResponse, GetDataList, GetDataListResponse, GoogleEntity
+
+from model.data_utils import CoNLLDataset
+from model.ner_model import NERModel
+from model.config import Config
+
+class PosTagger:
+    
+    def __init__(self):
+        
+        self.entities = {
+            '<patient_name>' : None,
+            '<address>' : None,
+            '<time>' : None,
+        }
+
+        self.srv_read_data = rospy.Service('%s/read_utterance'%rospy.get_name(), 
+                                    GoogleEntity, self.extract_entities)
+
+        config = Config()
+        
+        self.model = NERModel(config)
+        self.model.build()
+        self.model.restore_session(config.dir_model)
+
+        test = CoNLLDataset(config.filename_test, config.processing_word,
+                            config.processing_tag, config.max_iter)
+
+        self.model.evaluate(test)
+
+    
+    def extract_entities(self, utterance):
+        utterance = utterance.utterance
+
+        if utterance != '':
+            tokenized = utterance.lower().strip().split(" ")
+            preds = self.model.predict(tokenized)
+
+            for p in preds:
+                if p == 'B-PER' or p == 'I-PER':
+                    if self.entities['<patient_name>'] != None:
+                        self.entities['<patient_name>'] += ' ' + tokenized[preds.index(p)]
+                        tokenized.remove(tokenized[preds.index(p)])
+                    else:
+                        self.entities['<patient_name>'] = tokenized[preds.index(p)]
+                        tokenized = [r.replace(tokenized[preds.index(p)], 
+                                            '<patient_name>') for r in tokenized]
+                
+                elif p == 'B-LNUM' or p == 'B-LOC' or p == 'I-LOC':
+                    if self.entities['<address>'] != None:
+                        try:
+                            self.entities['<address>'] += ' ' + tokenized[preds.index(p)]
+                        except IndexError:
+                            self.entities['<address>'] += ' ' + tokenized[preds.index(p)-1]
+                        
+                        try:
+                            tokenized.remove(tokenized[preds.index(p)])
+                        except IndexError:
+                            tokenized.remove(tokenized[preds.index(p)-1])
+                    else:
+                        self.entities['<address>'] = tokenized[preds.index(p)]
+                        tokenized = [r.replace(tokenized[preds.index(p)], 
+                                            '<address>') for r in tokenized]
+                
+                elif p == 'B-TIME' or p == 'I-TIME':
+                    if self.entities['<time>'] != None:
+                        self.entities['<time>'] += ' ' + tokenized[preds.index(p)]
+                        tokenized.remove(tokenized[preds.index(p)])
+                    else:
+                        self.entities['<time>'] = tokenized[preds.index(p)]
+                        tokenized = [r.replace(tokenized[preds.index(p)], 
+                                            '<time>') for r in tokenized]
+        else:
+            rospy.logerr('utterance empty')
+        
+        entities = json.dumps(self.entities)
+        result_string = ' '.join(tokenized)
+
+        return entities, result_string
+        
+
+if __name__ == '__main__':
+    rospy.init_node('pos_tagging', anonymous=False)
+    p = PosTagger()
+    rospy.spin()
---
+++ src/train.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+#-*- encoding: utf8 -*-
+
+import rospkg
+import rospy
+warnings.filterwarnings('ignore', category=RuntimeWarning)
+
+from model.data_utils import CoNLLDataset
+from model.ner_model import NERModel
+from model.config import Config
+from model.data_utils import CoNLLDataset, get_vocabs, UNK, NUM, \
+    get_glove_vocab, write_vocab, load_vocab, get_char_vocab, \
+    export_trimmed_glove_vectors, get_processing_word
+
+class BuildData():
+
+    def __init__(self):
+        rospy.loginfo('\033[94m[build_data]\033[0m initialized.')
+        self.build()
+
+    def build(self):
+        # get config and processing of words
+        config = Config(load=False)
+        processing_word = get_processing_word(lowercase=True)
+
+        # generators
+        dev = CoNLLDataset(config.filename_dev, processing_word)
+        test = CoNLLDataset(config.filename_test, processing_word)
+        train = CoNLLDataset(config.filename_train, processing_word)
+
+        # build word and tag vocab
+        vocab_words, vocab_tags = get_vocabs([train, dev, test])
+        vocab_glove = get_glove_vocab(config.filename_glove)
+
+        vocab = vocab_words & vocab_glove
+        vocab.add(UNK)
+        vocab.add(NUM)
+
+        # save vocab
+        write_vocab(vocab, config.filename_words)
+        write_vocab(vocab_tags, config.filename_tags)
+
+        # trim GloVe Vectors
+        vocab = load_vocab(config.filename_words)
+        export_trimmed_glove_vectors(vocab, config.filename_glove,
+                                    config.filename_trimmed, config.dim_word)
+
+        # build and save char vocab
+        train = CoNLLDataset(config.filename_train)
+        vocab_chars = get_char_vocab(train)
+        write_vocab(vocab_chars, config.filename_chars)
+
+class Trainer():
+
+    def __init__(self):
+        b = BuildData()
+        
+        rospy.loginfo('\033[94m[%s]\033[0m initialized.'%rospy.get_name())
+        self.train()
+
+    def train(self):
+        config = Config()
+
+        # build model
+        model = NERModel(config)
+        model.build()
+
+        # create dataset
+        dev = CoNLLDataset(config.filename_dev, config.processing_word,
+                            config.processing_tag, config.max_iter)
+        train = CoNLLDataset(config.filename_train, config.processing_word,
+                            config.processing_tag, config.max_iter)
+        test = CoNLLDataset(config.filename_test, config.processing_word,
+                            config.processing_tag, config.max_iter)
+
+        model.train(train, dev)
+
+if __name__ == '__main__':
+    rospy.init_node('entity tagging', anonymous=False)
+    t = Trainer()
